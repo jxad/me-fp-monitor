fb17fe7632d88e9647c3b6ecea1cd5a7d08f2a17
jxad <jxad.games@gmail.com>
2022-06-03 14:33:37 +0200

    webhook error infinite loop fix

--- cmd/discord/utils.go
+++ cmd/discord/utils.go
@@ -16,15 +16,11 @@ func SendMessage(url string, message types.Message) {
 	err := json.NewEncoder(payload).Encode(message)
 	if err != nil {
 		fmt.Printf("Encoder error: %s\n", err)
-		LogError("Encoder error: " + err.Error())
-		return
 	}
 
 	resp, err := http.Post(url, "application/json", payload)
 	if err != nil {
 		fmt.Printf("Webhook req error: %s\n", err)
-		LogError("Webhook req error: " + err.Error())
-		return
 	}
 
 	if resp.StatusCode != 200 {
@@ -33,11 +29,8 @@ func SendMessage(url string, message types.Message) {
 		responseBody, err := io.ReadAll(resp.Body)
 		if err != nil {
 			fmt.Printf("decoding res error: %s\n", err)
-			LogError("decoding res error: " + err.Error())
-			return
 		}
 
 		fmt.Print(string(responseBody))
-		return
 	}
 }
